ab3bb39e70bc2e3a2c3047574a5c6628d60da8d7
tblanchet <tblanchet@switchfly.com>
2019-08-09 17:10:40 -0700

    build folder

---
+++ build/precache-manifest.abedc4e378f748eef4f94f0cf776d8cf.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f5e52fbd6df75c15bf1c65aee6f324e",
+    "url": "/pokedexito/index.html"
+  },
+  {
+    "revision": "f4ec58693b1e5be061cd",
+    "url": "/pokedexito/static/css/main.db23153d.chunk.css"
+  },
+  {
+    "revision": "e88d73d161cf5c9bfbe3",
+    "url": "/pokedexito/static/js/2.781ff5ca.chunk.js"
+  },
+  {
+    "revision": "f4ec58693b1e5be061cd",
+    "url": "/pokedexito/static/js/main.63089efb.chunk.js"
+  },
+  {
+    "revision": "d25149ff7a92accd68d0",
+    "url": "/pokedexito/static/js/runtime~main.7f2d25e8.js"
+  }
+]);
